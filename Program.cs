5ae90ac5c8a3d909b0754eaf66b84a3615a391be
Sebastian Morales Yabeta <a2018113411@estudiantes.upsa.edu.bo>
2021-05-25 12:38:14 -0400

    Adición del Análisis de Sentimientos

--- Program.cs
+++ Program.cs
@@ -14,10 +14,10 @@ namespace TextAnalyticsAzure
         {
             var client = new TextAnalyticsClient(endpoint, credentials);
             // Estas funciones se implementarán después.
-         /*   SentimentAnalysisExample(client);
+            SentimentAnalysisExample(client);
             Console.Write("Presiona una tecla para pasar a la Mineria de Opiniones.");
             Console.ReadKey();
-            SentimentAnalysisWithOpinionMiningExample(client);
+           /* SentimentAnalysisWithOpinionMiningExample(client);
             Console.Write("Presiona una tecla para pasar a la Detección de Lenguaje.");
             Console.ReadKey();
             LanguageDetectionExample(client);
@@ -39,7 +39,21 @@ namespace TextAnalyticsAzure
 
         }
 
+        static void SentimentAnalysisExample(TextAnalyticsClient client)
+        {
+            string inputText = "Tuve el peor día de mi vida";
+            DocumentSentiment documentSentiment = client.AnalyzeSentiment(inputText);
+            Console.WriteLine($"Document sentiment: {documentSentiment.Sentiment}\n");
 
+            foreach (var sentence in documentSentiment.Sentences)
+            {
+                Console.WriteLine($"\tTexto: \"{sentence.Text}\"");
+                Console.WriteLine($"\tSentimiento de la oración: {sentence.Sentiment}");
+                Console.WriteLine($"\tPuntaje Positivo: {sentence.ConfidenceScores.Positive:0.00}");
+                Console.WriteLine($"\tPuntaje Negativo: {sentence.ConfidenceScores.Negative:0.00}");
+                Console.WriteLine($"\tPuntaje Neutral: {sentence.ConfidenceScores.Neutral:0.00}\n");
+            }
+        }
 
 
     }
