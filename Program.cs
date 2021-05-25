b3ca802ec24ac47b6f7c635c27e6afa5ee1ebb3e
Sebastian Morales Yabeta <a2018113411@estudiantes.upsa.edu.bo>
2021-05-25 12:39:22 -0400

    Adición de la Minería de Opiniones

--- Program.cs
+++ Program.cs
@@ -17,10 +17,10 @@ namespace TextAnalyticsAzure
             SentimentAnalysisExample(client);
             Console.Write("Presiona una tecla para pasar a la Mineria de Opiniones.");
             Console.ReadKey();
-           /* SentimentAnalysisWithOpinionMiningExample(client);
+            SentimentAnalysisWithOpinionMiningExample(client);
             Console.Write("Presiona una tecla para pasar a la Detección de Lenguaje.");
             Console.ReadKey();
-            LanguageDetectionExample(client);
+            /*LanguageDetectionExample(client);
             Console.Write("Presiona una tecla para pasar al Reconocimiento de Entidades.");
             Console.ReadKey();
             EntityRecognitionExample(client);
@@ -55,6 +55,48 @@ namespace TextAnalyticsAzure
             }
         }
 
+        static void SentimentAnalysisWithOpinionMiningExample(TextAnalyticsClient client)
+        {
+            var documents = new List<string>
+        {
+        "The food and service were unacceptable, but the concierge were nice."
+        };
+
+            AnalyzeSentimentResultCollection reviews = client.AnalyzeSentimentBatch(documents, options: new AnalyzeSentimentOptions()
+            {
+                IncludeOpinionMining = true
+            });
+
+            foreach (AnalyzeSentimentResult review in reviews)
+            {
+                Console.WriteLine($"Sentimiento del Documento: {review.DocumentSentiment.Sentiment}\n");
+                Console.WriteLine($"\tPuntaje Positivo: {review.DocumentSentiment.ConfidenceScores.Positive:0.00}");
+                Console.WriteLine($"\tPuntaje Negativo: {review.DocumentSentiment.ConfidenceScores.Negative:0.00}");
+                Console.WriteLine($"\tPuntaje Neutro: {review.DocumentSentiment.ConfidenceScores.Neutral:0.00}\n");
+                foreach (SentenceSentiment sentence in review.DocumentSentiment.Sentences)
+                {
+                    Console.WriteLine($"\tTexto: \"{sentence.Text}\"");
+                    Console.WriteLine($"\tSentimiento de la Oración: {sentence.Sentiment}");
+                    Console.WriteLine($"\tPuntaje Positivo de la Oración: {sentence.ConfidenceScores.Positive:0.00}");
+                    Console.WriteLine($"\tPuntaje Negativo de la Oración: {sentence.ConfidenceScores.Negative:0.00}");
+                    Console.WriteLine($"\tPuntaje Neutro de la Oración: {sentence.ConfidenceScores.Neutral:0.00}\n");
+
+                    foreach (SentenceOpinion sentenceOpinion in sentence.Opinions)
+                    {
+                        Console.WriteLine($"\tObjetivo: {sentenceOpinion.Target.Text}, Valor: {sentenceOpinion.Target.Sentiment}");
+                        Console.WriteLine($"\tPuntaje Positivo de la Oración del Objetivo: {sentenceOpinion.Target.ConfidenceScores.Positive:0.00}");
+                        Console.WriteLine($"\tPuntaje Negativo de la Oración del Objetivo: {sentenceOpinion.Target.ConfidenceScores.Negative:0.00}");
+                        foreach (AssessmentSentiment assessment in sentenceOpinion.Assessments)
+                        {
+                            Console.WriteLine($"\t\tEvaluación: {assessment.Text}, Valor: {assessment.Sentiment}");
+                            Console.WriteLine($"\t\tPuntaje Positivo de la Evaluación: {assessment.ConfidenceScores.Positive:0.00}");
+                            Console.WriteLine($"\t\tPuntaje Negativo de la Evaluación: {assessment.ConfidenceScores.Negative:0.00}");
+                        }
+                    }
+                }
+                Console.WriteLine($"\n");
+            }
+        }
 
     }
 
